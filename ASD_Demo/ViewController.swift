9a77b925978cc2b9f50cda61f26b3b40cd43e78a
Tyler <tyler@burnamtech.com>
2016-10-11 10:45:15 -0400

    Initial Commit

---
+++ ASD_Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ASD_Demo
+//
+//  Created by Tyler Burnam on 10/11/16.
+//  Copyright © 2016 Dali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
